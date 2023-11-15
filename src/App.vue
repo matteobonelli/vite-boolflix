a5dbb23a9b4b27b281a3ddab0e53ad56de4ef57d
matteobonelli <matteobonelli08@gmail.com>
2023-11-15 12:20:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { store } from './assets/data/store'
+export default {
+  name: 'App',
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
